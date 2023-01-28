6f683be8483dc11176157a716c8b3de63552a6cd
Rayvd Kilo <rayvdKilo@gmail.com>
2023-01-28 13:06:18 +0300

    Append configuration data.

--- TeachersScheduleParser/Runtime/Structs/TelegramBotConfigurationData.cs
+++ TeachersScheduleParser/Runtime/Structs/TelegramBotConfigurationData.cs
@@ -13,17 +13,25 @@ public struct TelegramBotConfigurationData
 
     [JsonProperty] public readonly string StartupMessage;
 
+    [JsonProperty] public readonly string DataUpdateMessage;
+
+    [JsonProperty] public readonly string ClientRequestErrorMessage;
+
     [JsonProperty] public readonly string ErrorMessage;
 
     public TelegramBotConfigurationData(
         string botAccessToken,
         string nullDataMessage,
         string startupMessage,
-        string errorMessage)
+        string errorMessage,
+        string clientRequestErrorMessage, 
+        string dataUpdateMessage)
     {
         BotAccessToken = botAccessToken;
         NullDataMessage = nullDataMessage;
         StartupMessage = startupMessage;
+        DataUpdateMessage = dataUpdateMessage;
+        ClientRequestErrorMessage = clientRequestErrorMessage;
         ErrorMessage = errorMessage;
     }
 }
