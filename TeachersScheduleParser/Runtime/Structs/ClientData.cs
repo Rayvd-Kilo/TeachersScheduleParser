e77e2c0130be6bc8f0ecaac849b0b95ecc5af5f7
Rayvd Kilo <rayvdKilo@gmail.com>
2023-01-29 12:14:47 +0300

    Configure subscription mechanic.

--- TeachersScheduleParser/Runtime/Structs/ClientData.cs
+++ TeachersScheduleParser/Runtime/Structs/ClientData.cs
@@ -13,14 +13,18 @@ public struct ClientData
     public readonly long ChatId;
 
     [JsonProperty]
+    public readonly SubscriptionType SubscriptionType;
+
+    [JsonProperty]
     public readonly UpdateType UpdateType;
 
     [JsonProperty]
     public readonly string LastMessage;
 
-    public ClientData(long chatId, UpdateType updateType, string lastMessage)
+    public ClientData(long chatId, SubscriptionType subscriptionType, UpdateType updateType, string lastMessage)
     {
         ChatId = chatId;
+        SubscriptionType = subscriptionType;
         UpdateType = updateType;
         LastMessage = lastMessage;
     }
